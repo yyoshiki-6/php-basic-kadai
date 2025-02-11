0dc4c98f95f6baf2775a64edd22b74d49227750b
yyoshiki-6 <yy6br@icloud.com>
2025-02-11 23:43:58 +0900

    kadai_007をアップデート

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>連想配列を使ってキーと値を出力しよう</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    // 変数に代入する
+    $personal_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    // 値を出力する
+    print_r($personal_data);
+    ?>
+  </p>
+</body>
+</html>
